0e6a2737dcbeee24a186650a23530f25d2fa7737
Hyejin Yoon <hjyoon2650@naver.com>
2025-07-19 02:00:32 +0900

    input_data_land_only.csv로 수정

--- controller/predict_controller.py
+++ controller/predict_controller.py
@@ -1,7 +1,17 @@
+import traceback
 from dto.predict_dto import PredictRequest
 from service.predict_service import process_prediction
 
-async def predict_fire(req: PredictRequest):  # ✅ async로 변경
-    return await process_prediction(req.lat, req.lon)  # ✅ await 사용
+async def predict_fire(req: PredictRequest):
+    try:
+        return await process_prediction(req.lat, req.lon)
+    except Exception as e:
+        print("🔥 예측 처리 중 예외 발생:", e)
+        traceback.print_exc()
+        return {"error": str(e)}
+
+
+
+
 
 
